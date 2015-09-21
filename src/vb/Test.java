7c9c9a2631c1510b129f243b592f06299762ecde
陈宁 <chenning@olymtech.com>
2015-09-21 15:24:38 +0800

    1

--- src/vb/Test.java
+++ src/vb/Test.java
@@ -1,9 +1,29 @@
 package vb;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+		System.out.println(System.getenv("SHELL"));
+		 Map m = System.getenv();
+         for ( Iterator it = m.keySet().iterator(); it.hasNext(); )
+         {
+                String key = (String ) it.next();
+                String value = (String )  m.get(key);
+                System.out.println(key +":" +value);
+         }
+         System.out.println( "--------------------------------------" );
+         Properties p = System.getProperties();
+        
+         for ( Iterator it = p.keySet().iterator(); it.hasNext(); )
+         {
+                String key = (String ) it.next();
+                String value = (String )  p.get(key);
+                System.out.println(key +":" +value);
+         }
 
 	}
 
